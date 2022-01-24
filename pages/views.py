a358102b1df074f95b5425aa32bf8f6269444647
joshuadommy <bartazarjoshua@gmail.com>
2022-01-24 15:12:25 +0300

    market place updated

--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,7 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
+
+
+class ComingSoonView(TemplateView):
+    template_name = 'comingsoon.html'
